c6d8021f8c8d32b7892d3ac4858b804fa277b9fa
Vladimir Jeftovic <vjeftovic@gmail.com>
2023-10-26 12:58:48 +0200

    missing flat list component

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { MoveoOne } from "./src/MoveoOne";
 import { MoveoButton } from "./src/MoveoButton";
 import { MoveoText } from "./src/MoveoText";
 import { MoveoTextInput } from "./src/MoveoTextInput";
+import { MoveoFlatList } from "./src/MoveoFlatList";
 import { KEYS, TYPE, ACTION } from './src/MoveoKeys';
 
-export { MoveoOne, MoveoButton, MoveoText, MoveoTextInput, KEYS, TYPE, ACTION };
+export { MoveoOne, MoveoButton, MoveoText, MoveoTextInput, MoveoFlatList, KEYS, TYPE, ACTION };
